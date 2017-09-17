5ae80cf8286f449fbc5268e5955bc41a6d6c96b0
coon <coon@c-base.org>
2017-09-17 12:06:37 +0200

    core main: remove old sample code

--- core/core_main.c
+++ core/core_main.c
@@ -13,26 +13,16 @@
 #define mainREGION_2_SIZE	18105
 #define mainREGION_3_SIZE	2807
 
-#define mainQUEUE_RECEIVE_TASK_PRIORITY	(tskIDLE_PRIORITY + 2)
-#define	mainQUEUE_SEND_TASK_PRIORITY		(tskIDLE_PRIORITY + 1)
-#define mainTASK_SEND_FREQUENCY_MS			pdMS_TO_TICKS(200UL)
-#define mainTIMER_SEND_FREQUENCY_MS			pdMS_TO_TICKS(2000UL)
-#define mainQUEUE_LENGTH					      (10)
+#define mainQUEUE_DEFAULT_TASK_PRIORITY	(tskIDLE_PRIORITY + 1)
 #define blinkTASK_FREQUENY_MS     			pdMS_TO_TICKS(100UL)
 
-static void _prvQueueReceiveTask(void *pParameters);
-static void _prvQueueSendTask(void *pParameters);
-static void _prvQueueSendTimerCallback(TimerHandle_t xTimerHandle);
 static void prvInitialiseHeap( void );
-
-static QueueHandle_t _xQueue = NULL;
-static TimerHandle_t _xTimer = NULL;
-static SemaphoreHandle_t _xTaskMutex = NULL;
 
 static void _prvBlinkTask(void *pParameters) {
   (void*)pParameters;
 
   while (1) {
+    myprintf("blink\n");
     statusLedOn();
     vTaskDelay(blinkTASK_FREQUENY_MS);
     statusLedOff();
@@ -48,8 +38,8 @@ static void _prvDebugTask(void* pParameters) {
   (void*)pParameters;
 
   for(int i = 1000;; i++) {
-    vTaskDelay(pdMS_TO_TICKS(10UL));
-    // myprintf("Tick: %i\n", i);
+    vTaskDelay(pdMS_TO_TICKS(250UL));
+    myprintf("Tick: %i\n", i);
   }
 }
 
@@ -154,88 +144,26 @@ static void _prvDebugTask(void* pParameters) {
 int coreMain(void) {
   prvInitialiseHeap();
   statusLedOff();
-  xTaskCreate(_prvBlinkTask,      "Blink Task", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_RECEIVE_TASK_PRIORITY, NULL);
-  xTaskCreate(_prvDebugTask,      "Debug Task", 512, NULL, mainQUEUE_RECEIVE_TASK_PRIORITY, NULL);
+  xTaskCreate(_prvBlinkTask,      "Blink Task", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_DEFAULT_TASK_PRIORITY, NULL);
+  xTaskCreate(_prvDebugTask,      "Debug Task", 512, NULL, mainQUEUE_DEFAULT_TASK_PRIORITY, NULL);
   // xTaskCreate(_prvFileSystemTask, "File System Task", 1024, NULL, mainQUEUE_RECEIVE_TASK_PRIORITY, NULL);
   // xTaskCreate(_prvGamePadTask,      "Game pad Task", 512, NULL, mainQUEUE_RECEIVE_TASK_PRIORITY, NULL);
 
   vTaskStartScheduler();
 
-  if(true)
-    return 0;
-
-
-
-  const TickType_t xTimerPeriod = mainTIMER_SEND_FREQUENCY_MS;
-  _xQueue = xQueueCreate(mainQUEUE_LENGTH, sizeof(uint32_t));
-
-  if (_xQueue != NULL) {
-    xTaskCreate(_prvQueueReceiveTask, "Rx", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_RECEIVE_TASK_PRIORITY, NULL);
-
-    uint32_t vt1[] = {1, 2, 3};
-    uint32_t vt2[] = {4, 5, 6};
-
-    _xTaskMutex = xSemaphoreCreateMutex();
-    xTaskCreate(_prvQueueSendTask, "Tx1", configMINIMAL_STACK_SIZE, vt1, mainQUEUE_SEND_TASK_PRIORITY, NULL);
-    xTaskCreate(_prvQueueSendTask, "Tx2", configMINIMAL_STACK_SIZE, vt2, mainQUEUE_SEND_TASK_PRIORITY, NULL);
-
-    _xTimer = xTimerCreate("Timer", xTimerPeriod, pdFALSE, NULL, _prvQueueSendTimerCallback);
-
-    vTaskStartScheduler();
-  }
-
   return 0;
 }
 
-static void _prvQueueSendTask(void *pParameters) {
-  uint32_t pValuesToSend[3];
-
-  for (int i = 0; i < 3; ++i)
-    pValuesToSend[i] = ((uint32_t*)pParameters)[i];
-
-  TickType_t xNextWakeTime = xTaskGetTickCount();
-  const TickType_t xBlockTime = mainTASK_SEND_FREQUENCY_MS;
-
-  for(;;) {
-    vTaskDelayUntil(&xNextWakeTime, xBlockTime);
-    xSemaphoreTake(_xTaskMutex, portMAX_DELAY);
-
-    int numElems = sizeof(pValuesToSend) / sizeof(uint32_t);
-
-    for(int i = 0; i < numElems; ++i)
-      xQueueSend(_xQueue, &pValuesToSend[i], 0U);
-
-    xSemaphoreGive(_xTaskMutex);
-  }
-}
-
-static void _prvQueueSendTimerCallback(TimerHandle_t xTimerHandle) {
-  const uint32_t ulValueToSend = 3;
-  (void) xTimerHandle;
-
-  xQueueSend(_xQueue, &ulValueToSend, 0U);
-}
-
-static void _prvQueueReceiveTask(void *pParameters) {
-  uint32_t ulReceivedValue;
-  (void) pParameters;
-
-  for(;;) {
-    xQueueReceive(_xQueue, &ulReceivedValue, portMAX_DELAY);
-    myprintf("Value: %lu\n", ulReceivedValue);
-  }
-}
-
-static void  prvInitialiseHeap(void) {
+static void prvInitialiseHeap(void) {
   static uint8_t ucHeap[configTOTAL_HEAP_SIZE];
   volatile uint32_t ulAdditionalOffset = 19;
 
   const HeapRegion_t xHeapRegions[] = {
-  { ucHeap + 1,   mainREGION_1_SIZE },
-  { ucHeap + 15 + mainREGION_1_SIZE, mainREGION_2_SIZE },
-  { ucHeap + 19 + mainREGION_1_SIZE + mainREGION_2_SIZE, mainREGION_3_SIZE },
-  { NULL, 0 }
-};
+    { ucHeap + 1,   mainREGION_1_SIZE },
+    { ucHeap + 15 + mainREGION_1_SIZE, mainREGION_2_SIZE },
+    { ucHeap + 19 + mainREGION_1_SIZE + mainREGION_2_SIZE, mainREGION_3_SIZE },
+    { NULL, 0 }
+  };
 
   configASSERT((ulAdditionalOffset + mainREGION_1_SIZE + mainREGION_2_SIZE + mainREGION_3_SIZE ) < configTOTAL_HEAP_SIZE);
 
