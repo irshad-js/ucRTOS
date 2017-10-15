9f2ba429221814d7dc45b206d7e4086ede54e010
coon <coon@c-base.org>
2017-10-15 22:14:01 +0200

    display.h: fix declaration order

--- core/display.h
+++ core/display.h
@@ -6,10 +6,10 @@ void displayInit();
 void displaySetPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
 void displayDraw();
 void displayClear();
+void displayDrawImage(uint16_t xPos, uint16_t yPos, const uint8_t* pImg);
 
 // Platform driver implementations:
 void hardwareDisplayInit();
 void hardwareDisplaySetPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
 void hardwareDisplayDraw();
 void hardwareDisplayClear();
-void displayDrawImage(uint16_t xPos, uint16_t yPos, const uint8_t* pImg);
