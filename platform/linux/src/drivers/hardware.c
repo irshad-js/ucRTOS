02e134b87359d15329e50d92242be4478085e598
coon <coon@mailbox.org>
2018-04-17 19:01:35 +0200

    add platform linux

---
+++ platform/linux/src/drivers/hardware.c
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <stdio.h>
+#include "../../core/ucrtos.h"
+
+void statusLedOn() {
+  if (GetKeyState(VK_SCROLL) == 1)
+    return;
+
+  keybd_event(VK_SCROLL, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
+  keybd_event(VK_SCROLL, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+}
+
+void statusLedOff() {
+  if (GetKeyState(VK_SCROLL) == 0)
+    return;
+
+  keybd_event(VK_SCROLL, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
+  keybd_event(VK_SCROLL, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+}
+
+void PrintCharUsr(char c) {
+  putchar(c);
+}
