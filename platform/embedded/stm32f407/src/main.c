a958f7501471d48111e7acda5bfd0794d2d9448e
coon <coon@c-base.org>
2018-05-27 16:44:29 +0200

    platform stm32f4: init RS485 bus UART

--- platform/embedded/stm32f407/src/main.c
+++ platform/embedded/stm32f407/src/main.c
@@ -62,9 +62,38 @@ static void initDebugUart(uint32_t baudrate) {
   while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
 }
 
+static void initBusUart(uint32_t baudrate) {
+  GPIO_InitTypeDef GPIO_InitStruct;
+  USART_InitTypeDef USART_InitStruct;
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART6, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_USART6);
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_USART6);
+
+  USART_InitStruct.USART_BaudRate = baudrate;
+  USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+  USART_InitStruct.USART_StopBits = USART_StopBits_1;
+  USART_InitStruct.USART_Parity = USART_Parity_No;
+  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+  USART_Init(USART6, &USART_InitStruct);
+
+  USART_Cmd(USART6, ENABLE);
+}
+
 static void init() {
   initLeds();
   initDebugUart(9600);
+  initBusUart(9600);
 
   // CHECKME: why is SystemCoreClock divided by 1000 here? It seems to be required for
   // FreeRTOS but USB OTG does not work when doing that:
