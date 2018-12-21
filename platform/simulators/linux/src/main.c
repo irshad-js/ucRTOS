0a1a3079603c89504d4b11f9ad522b39da73274d
coon <coon@mailbox.org>
2018-12-21 22:16:45 +0100

    platform linux: compile FreeRTOS sources

--- platform/simulators/linux/src/main.c
+++ platform/simulators/linux/src/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char* pArgv[]) {
   printf("ucRTOS linux simulator\n");
 
   return 0;
+}
+
+//-----------------------------------------------------------------------------
+// FreeRTOS Application Hooks
+//-----------------------------------------------------------------------------
+
+void vApplicationIdleHook(void) {
+  // The co-routines are executed in the idle task using the idle task hook.
+  // vCoRoutineSchedule(); // Comment this out if not using Co-routines.
+
+  // Makes the process more agreeable when using the Posix simulator:
+  struct timespec xTimeToSleep, xTimeSlept;
+  xTimeToSleep.tv_sec = 1;
+  xTimeToSleep.tv_nsec = 0;
+  nanosleep(&xTimeToSleep, &xTimeSlept);
+}
+
+void vMainQueueSendPassed(void) {
+
 }
 
