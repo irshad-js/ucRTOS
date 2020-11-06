b537cda30b3fe51e8939244e0571a55288df571c
coon <coon@mailbox.org>
2020-11-06 21:27:48 +0100

    platform linux: update run_ucrtos.sh to build wx if required

--- platform/simulators/linux/run_ucrtos.sh
+++ platform/simulators/linux/run_ucrtos.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+export WXDIR=$PWD/../lib/wxWidgets/gtk-build
+
+if [ -d $WXDIR ]
+then
+    echo "Directory $WXDIR exists."
+else
+    echo "Error: Directory $WXDIR does not exists."
+
+    mkdir ../lib/wxWidgets/gtk-build
+    pushd ../lib/wxWidgets/gtk-build
+    ../configure --prefix=$PWD/../lib/gtk3_so --with-gtk=3 --with-opengl
+    make -j4
+    make install
+    popd
+fi
+
 export PATH="$PATH:$PWD/../lib/wxWidgets/gtk-build"
 
 make
